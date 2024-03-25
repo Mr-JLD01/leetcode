1c54672b984efe9c2f57c265a09361354eb90081
Mr-JLD01 <57381310+Mr-JLD01@users.noreply.github.com>
2024-03-24 20:46:55 -0500

    Delete power-of-three directory

--- power-of-three/power-of-three.py
+++
@@ -1,8 +0,0 @@
-class Solution:
-    def isPowerOfThree(self, n: int) -> bool:
-        for i in range(n):
-            if 3**i > n:
-                return False
-            if 3**i == n:
-                return True
-        
