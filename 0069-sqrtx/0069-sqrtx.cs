1136d84952135f9d020dbd209724a3faf6953079
Mr-JLD01 <57381310+Mr-JLD01@users.noreply.github.com>
2024-07-08 22:15:45 -0500

    Time: 23 ms (62.98%), Space: 27 MB (55.90%) - LeetHub

---
+++ 0069-sqrtx/0069-sqrtx.cs
@@ -0,0 +1,22 @@
+public class Solution {
+    public int MySqrt(int x) {
+        if (x == 0 || x == 1)
+            return x;
+
+        int start = 1;
+        int end = x;
+        int mid = -1;
+
+        while (start <= end) {
+            mid = start + (end - start) / 2;
+
+            if ((long) mid * mid > (long) x)
+                end = mid - 1;
+            else if ((long)mid * mid == (long)x)
+                return mid;
+            else
+                start = mid + 1;
+        }
+        return end;
+    }
+}
